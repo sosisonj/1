e4c7f7eaf9d478a0bcccb45b31843ebf82b85fac
sosisonj <88275713+sosisonj@users.noreply.github.com>
2021-08-01 15:48:28 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -157,7 +157,7 @@ local library = {
     pointers = {},
     settings = {
         guiname = "vovality",
-		title = "geomayka stop crying",
+		title = "slabousware",
 		logo = "rbxassetid://4350178803", -- "http://www.roblox.com/asset/?id=6597976562",
         footer = "я курю говно",
         modal = true,
